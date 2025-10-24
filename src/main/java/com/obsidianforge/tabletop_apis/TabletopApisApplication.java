db8ca53cb8cf9614445bee904ef6c60b3e92080e
harbargerdev <codeamber0621@pm.me>
2025-10-24 16:26:13 -0400

    Spring Initializr + DevContainer + GitHub

---
+++ src/main/java/com/obsidianforge/tabletop_apis/TabletopApisApplication.java
@@ -0,0 +1,13 @@
+package com.obsidianforge.tabletop_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletopApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabletopApisApplication.class, args);
+	}
+
+}
